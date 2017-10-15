e11d0acf8b8fd413bdc7a5b2d58eb8c16c3b3055
small44 <ismailghedamsi@gmail.com>
2017-10-15 11:43:02 -0500

    -Documenter le package bean

--- TP3/src/cal/java03/tp3/bean/ClientEntreprise.java
+++ TP3/src/cal/java03/tp3/bean/ClientEntreprise.java
@@ -3,19 +3,88 @@ package cal.java03.tp3.bean;
 
 
 
-
+/**
+ * Classe representant un actionnaire de type Entreprise
+ * @author small44
+ *
+ */
 public class ClientEntreprise extends Client {
 	private static int compteur;
 	private  int idClientEntreprise;
 	private String nomEntreprise;
 	private String cote;
 	private int nombreEmploye;
+	/**
+	 * Constructeur client entreprise
+	 * @param etatCompte
+	 * @param email
+	 * @param nomEntreprise
+	 * @param cote
+	 * @param nombreEmploye
+	 */
 	public ClientEntreprise(String etatCompte, String email, String nomEntreprise,
 			String cote, int nombreEmploye) {
 		super(email);
 		this.nomEntreprise = nomEntreprise;
 		this.cote = cote;
 		this.nombreEmploye = nombreEmploye;
+		this.idClientEntreprise =compteur;
+		compteur++;
 	}
+	
+	/**
+	 * 
+	 * @return le nom de l'entrprise
+	 */
+	public String getNomEntreprise() {
+		return nomEntreprise;
+	}
+	
+	/**
+	 * Modificateur du nom de l'entreprise
+	 * @param nomEntreprise
+	 */
+	public void setNomEntreprise(String nomEntreprise) {
+		this.nomEntreprise = nomEntreprise;
+	}
+	
+	/**
+	 * 
+	 * @return Le cote de l'entreprise
+	 */
+	public String getCote() {
+		return cote;
+	}
+	/**
+	 * Modificateur de la cote de l'entrprise
+	 * @param cote
+	 */
+	public void setCote(String cote) {
+		this.cote = cote;
+	}
+	
+	/**
+	 * 
+	 * @return le nombre d'employe impliqués dans le portefeuille.
+	 */
+	public int getNombreEmploye() {
+		return nombreEmploye;
+	}
+	/**
+	 * Modificateur du nombre d'employe impliqués dans le portefeuille.
+	 * @param nombreEmploye
+	 */
+	public void setNombreEmploye(int nombreEmploye) {
+		this.nombreEmploye = nombreEmploye;
+	}
+	
+	/**
+	 * 
+	 * @return l'id du client
+	 */
+	public int getIdClientEntreprise() {
+		return idClientEntreprise;
+	}
+	
 	
 }
--- TP3/src/cal/java03/tp3/bean/Portefeuille.java
+++ TP3/src/cal/java03/tp3/bean/Portefeuille.java
@@ -3,6 +3,11 @@ package cal.java03.tp3.bean;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Classe representant un compte client pour effectuer des transations
+ * @author small44
+ *
+ */
 public class Portefeuille {
 	private static int compteur;
 	private List<Action> listeAction;
@@ -10,6 +15,13 @@ public class Portefeuille {
 	private int idClient;
 	private String typePortefeuille;
 	private float argentInvesti;
+	
+	/**
+	 * Contructeur d'un portefeuille
+	 * @param idClient
+	 * @param typePortefeuille
+	 * @param argentInvesti
+	 */
 	public Portefeuille(int idClient, String typePortefeuille,float argentInvesti ) {
 		super();
 		this.idClient = idClient;
@@ -17,33 +29,67 @@ public class Portefeuille {
 		this.listeAction = new ArrayList<Action>();
 		this.argentInvesti = argentInvesti;
 	}
-	public static int getCompteur() {
-		return compteur;
-	}
-	public static void setCompteur(int compteur) {
-		Portefeuille.compteur = compteur;
-	}
+
+	/**
+	 * 
+	 * @return la liste des actions dans un portefeuille
+	 */
 	public List<Action> getListeAction() {
 		return listeAction;
 	}
+	
+	/**
+	 * Modificateur de la liste d'action
+	 * @param listeAction
+	 */
 	public void setListeAction(List<Action> listeAction) {
 		this.listeAction = listeAction;
 	}
+	
+	/**
+	 * 
+	 * @return le type du portefeuille : national ou international
+	 */
 	public String getTypePortefeuille() {
 		return typePortefeuille;
 	}
+	
+	/**
+	 * Modificateur du type de portefeuille
+	 * @param typePortefeuille
+	 */
 	public void setTypePortefeuille(String typePortefeuille) {
 		this.typePortefeuille = typePortefeuille;
 	}
+	
+	/**
+	 * 
+	 * @return l'argent investi
+	 */
 	public float getArgentInvesti() {
 		return argentInvesti;
 	}
+	
+	/**
+	 * Modificateur de l'argent investi
+	 * @param argentInvesti
+	 */
 	public void setArgentInvesti(float argentInvesti) {
 		this.argentInvesti = argentInvesti;
 	}
+	
+	/**
+	 * 
+	 * @return l'id du portefeuille
+	 */
 	public int getIdPortefeuille() {
 		return idPortefeuille;
 	}
+	
+	/**
+	 * l'id du client
+	 * @return
+	 */
 	public int getIdClient() {
 		return idClient;
 	}
